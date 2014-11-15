3a9d340262fc95c3005c6dc009dacbe6bb48491e
kunhuang110 <kunhuang110@126.com>
2014-11-15 11:08:40 -0800

    sample connection data

--- mysite/hackathon/views.py
+++ mysite/hackathon/views.py
@@ -12,12 +12,15 @@ def test(request):
     payload = {'grant_type': 'authorization_code',
                'code': request.GET['code'],
                'redirect_uri': 'http://localhost:8000/hackathon/test',
-               'scope': 'r_fullprofile',
+               'scope': 'r_network r_fullprofile',
                'client_id': '75rlqt9xns8nr4',
                'client_secret': 'U1Ogc52WYwIuTuBn'}
     r = requests.get(url, params=payload)
 
-    url = 'https://api.linkedin.com/v1/people/url={https%3A%2F%2Fwww.linkedin.com%2Fin%2Fvyasbhairavi}:(first-name,last-name,headline,picture-url,id,skills)?format=json'
+    url = 'https://api.linkedin.com/v1/people/~/connections:(first-name,last-name,headline,picture-url,id,skills)?format=json'
+    # url = 'https://api.linkedin.com/v1/people/url={https%3A%2F%2Fwww.linkedin.com%2Fin%2Fmenglonghu}:(first-name,last-name,headline,picture-url,id,skills)?format=json'
+    # url = 'https://api.linkedin.com/v1/people/id=KbyF0yU1Iw:(first-name,last-name,headline,picture-url,id,skills,public-profile-url)?format=json'
+    
     headers = {'content-type': 'application/json'}
     payload = {'oauth2_access_token' : json.loads(r.content)['access_token']}
     r = requests.get(url, params=payload, headers=headers)
